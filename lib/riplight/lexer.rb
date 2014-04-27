eff3946fd596355820d3fd9fb945890ee30c2e37
David Grayson <davidegrayson@gmail.com>
2014-04-27 13:34:53 -0700

    Made it handle __END__ correctly.

--- lib/riplight/lexer.rb
+++ lib/riplight/lexer.rb
@@ -2,27 +2,52 @@ require 'ripper'
 
 module Riplight
   module Lexer
+    # @param source (String)
+    # @param filename (String)
+    # @param line_number (Integer)
     def self.lex(source, filename = '-', line_number = 1)
       ripper_tokens = Ripper.lex(source, filename, line_number)
-      ripper_tokens.map do |ripper_token|
+      tokens = ripper_tokens.map do |ripper_token|
         convert_ripper_token ripper_token
       end
+      add_end_data(source, ripper_tokens, tokens)
+      tokens
     end
-    
+
     def self.convert_ripper_token(ripper_token)
       coords, ripper_token_type, string = ripper_token
       [string, token_type(ripper_token_type)]
     end
-    
+
     def self.token_type(ripper_token_type)
       case ripper_token_type
       when :on_const then :constant
-      when :on_kw then :keyword
+      when :on_kw, :on___end__ then :keyword
       when :on_period then :period
-      when :on_sp, :on_ignored_nl then :space
+      when :on_sp, :on_ignored_nl, :on_nl then :space
       when :on_ident then :identifier
       else ripper_token_type
       end
+    end
+
+    private
+
+    def self.add_end_data(source, ripper_tokens, tokens)
+      last_token = ripper_tokens.last
+      return unless last_token[1] == :on___end__
+      end_row, end_column = last_token[0]
+      pp [end_column, last_token[2].size]
+      data_column = end_column + last_token[2].size
+      data = string_starting_at_coords(source, end_row, data_column)
+
+      if !data.empty?
+        tokens << [data, :end_data]
+      end
+    end
+
+    def self.string_starting_at_coords(string, row, column)
+      regexp = Regexp.new("(?:.*?\n){#{row - 1}}.{#{column}}(.*)", Regexp::MULTILINE)
+      string.slice(regexp, 1)
     end
   end
 end
--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
+# TODO: make sure we handle everything in Ripper::SCANNER_EVENTS
+# TODO: describe encoding issues
+# TODO: describe newline issues
+# TODO: describe trailing and leading whitespace issues
+
 describe Riplight::Lexer do
+  def lex(*args)
+    described_class.lex *args
+  end
+
   it 'identifies whitespace' do
-    expect(described_class.lex(" \n\t  ")).to eq [
+    expect(lex(" \n\t  ")).to eq [
       [" ", :space],
       ["\n", :space],
       ["\t  ", :space],
@@ -10,21 +19,22 @@ describe Riplight::Lexer do
   end
 
   keywords = %w{
+    BEGIN END __ENCODING__ __END__ __FILE__ __LINE__
     alias and begin break case class def defined? do else elsif end ensure false
     for if in module next nil not or redo rescue retry return self super then
     true undef unless until when while yield
   }
-  
+
   keywords.each do |keyword|
     it "identifies #{keyword} as a keyword" do
-      expect(described_class.lex(keyword)).to eq [[keyword, :keyword]]    
+      expect(lex(keyword)).to eq [[keyword, :keyword]]
     end
   end
-  
+
   it 'identifies period' do
-    expect(described_class.lex('.')).to eq [['.', :period]]
+    expect(lex('.')).to eq [['.', :period]]
   end
-  
+
   it 'identifies method calls as a identifiers even if they look like keywords' do
     expect(described_class.lex('self.return')).to eq [
       ["self", :keyword],
@@ -32,12 +42,40 @@ describe Riplight::Lexer do
       ["return", :identifier],
     ]
   end
-  
+
   it "identifies constants" do
-    expect(described_class.lex('ABC')).to eq [['ABC', :constant]]
+    expect(lex('ABC')).to eq [['ABC', :constant]]
   end
-  
+
+  it 'identifies constants in class definitions' do
+    expect(lex('class A')).to eq [
+      ['class', :keyword],
+      [' ', :space],
+      ['A', :constant],
+    ]
+  end
+
   it "identifies constants that are actually method calls", flaw: true do
-    expect(described_class.lex('A()')[0]).to eq ['A', :constant]
+    expect(lex('A()')[0]).to eq ['A', :constant]
+  end
+
+  describe '__END__ keyword' do
+    it 'identifies __END__ and includes the data after it' do
+      expect(lex("hi\n__END__\ndata1\ndata2")).to eq [
+        ['hi', :identifier],
+        ["\n", :space],
+        ["__END__\n", :keyword],
+        ["data1\ndata2", :end_data]
+      ]
+    end
+
+    it 'does not recognize __END__ unless it is on its own line' do
+      expect(lex("hi\n__END__ ")).to eq [
+        ['hi', :identifier],
+        ["\n", :space],
+        ['__END__', :identifier],
+        [' ', :space],
+      ]
+    end
   end
 end
